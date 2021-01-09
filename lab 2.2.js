502adb39dc3d187a6ca7760d12de2e2852521913
1mperix <zakabaka133@gmail.com>
2021-01-09 18:35:30 +0200

    2.2

---
+++ lab 2.2.js
@@ -0,0 +1,12 @@
+function range(start, end){
+  let array = [];
+  for(let i = start; i <= end; i++){
+    array.push(i);
+  }
+  return array;
+}
+
+console.log(range(15, 30));
+
+const rangeOdd = (start, end) => range(start, end).filter(el => el % 2);
+console.log(rangeOdd(15,30));
