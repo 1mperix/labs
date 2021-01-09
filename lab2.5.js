d69d108bde1da6a2db58ae95c44ac70a018a2ffc
1mperix <zakabaka133@gmail.com>
2021-01-09 19:23:14 +0200

    labs

---
+++ lab2.5.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const phonesBook = [
+  {name: 'Marcus Aurelius', phone: '+380445554433'},
+  {name: 'Lombard', phone: '+88005553535'},
+]
+const findPhoneByName = (name) => {
+  for(let keyword of phonesBook){
+    if (name === keyword.name){
+      return keyword.phone;
+    }
+  }
+  return 'This name is not defined';
+}
+
+console.log(findPhoneByName(`Marcus Aurelius`));
+
+const hash = {
+  'Marcus Aurelius': '+380445554433',
+  'Lombard': '+88005553535',
+}
+
+const findPhoneByNameHash = (name) => hash[name];
+
+console.log(findPhoneByNameHash('Lombard'));
